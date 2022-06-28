d22986103340ac3e640af5c243120f8d82aa7e73
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-06-28 12:00:46 +0700

    Update D.cpp

--- De 2022 - 2023 (Tran Anh Tuan)/D.cpp
+++ De 2022 - 2023 (Tran Anh Tuan)/D.cpp
@@ -4,31 +4,34 @@ typedef long long ll;
 
 int main()
 {
-    ll n;
-    cin >> n;
-    ll a[n];
-    for (ll i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    stack<ll> s;
-    ll res = 0;
-    ll i = 0;
-    while(i < n){
-        if (s.empty() || a[s.top()] <= a[i]){
-            s.push(i++);
+    while(1){
+        ll n;
+        cin >> n;
+        if (n == 0) break;
+        ll a[n];
+        for (ll i = 0; i < n; i++){
+            cin >> a[i];
         }
-        else{
+        stack<ll> s;
+        ll res = 0;
+        ll i = 0;
+        while(i < n){
+            if (s.empty() || a[s.top()] <= a[i]){
+                s.push(i++);
+            }
+            else{
+                ll t = s.top();
+                s.pop();
+                if (s.empty()) res = max(res, a[t] * i);
+                else res = max(res, a[t] * (i - s.top() - 1));
+            }
+        }
+        while(!s.empty()){
             ll t = s.top();
             s.pop();
             if (s.empty()) res = max(res, a[t] * i);
             else res = max(res, a[t] * (i - s.top() - 1));
         }
+        cout << res << '\n';
     }
-    while(!s.empty()){
-        ll t = s.top();
-        s.pop();
-        if (s.empty()) res = max(res, a[t] * i);
-        else res = max(res, a[t] * (i - s.top() - 1));
-    }
-    cout << res;
 }
