8617584d18b27b3c7ecbcc183456e6179f3dd1c2
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-07-06 18:52:20 +0700

    Create A.cpp

---
+++ Codeforces/Testing Round 4/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string str;
+typedef pair <int,int> ii;
+#define file "SXOR"
+#define st first
+#define nd second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define er equal_range
+#define bin binary_search
+#define vll vector<ll>
+#define vi vector<int>
+#define all(v) (v).begin(), (v).end()
+#define FOR(i,x,y) for(ll i = x; i <= y; ++i)
+#define FOS(i,x,y) for(ll i = x; i >= y; --i)
+#define EACH(i,x) for (auto &(i) : (x))
+#define el cout << '\n';
+const ll MOD = 1e9 + 7;
+#define dbg(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]  "
+#define dbge(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl;
+mt19937_64 rd(chrono::steady_clock::now().time_since_epoch().count());
+ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rd); }
+
+const ll N = 1e5 + 5;
+ll n, res = 0;
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    // freopen(file".INP","r",stdin);
+    // freopen(file".OUT","w",stdout);
+    
+    // cin >> n;
+    FOR(n,1,20){
+        res = 0;
+        for (ll i = 0; i <= n; i++){
+            for (ll j = 0; j <= n; j++){
+                if (i + j == (i ^ j)) {
+                    res++;
+                }
+            }
+        }
+        cout << res << ' ';
+    }
+    
+   
+    
+    return 0;
+}
