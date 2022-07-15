44a1f943eb5f331a6e32fd41679fef45af91ce45
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-07-15 18:57:47 +0700

    Create E.cpp

---
+++ Codeforces/Contest NMT/E.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string str;
+typedef pair <int,int> ii;
+#define file "TEST"
+#define st first
+#define nd second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define er equal_range
+#define bin binary_search
+#define vll vector<ll>
+#define vi vector<int>
+#define all(v) (v).begin(), (v).end()
+#define FOR(i,x,y) for(ll i = x; i <= y; ++i)
+#define FOS(i,x,y) for(ll i = x; i >= y; --i)
+#define EACH(i,x) for (auto &(i) : (x))
+#define el cout << '\n';
+const ll MOD = 1e9 + 7;
+#define dbg(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]  "
+#define dbge(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl;
+mt19937_64 rd(chrono::steady_clock::now().time_since_epoch().count());
+ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rd); }
+
+
+const ll N = 111;
+ 
+ll n;
+
+struct Matrix {
+
+int rowSize = N - 1;
+int columnSize = N - 1;
+int m[N][N] = {};
+
+Matrix operator * (Matrix b) {
+    Matrix C;
+    for (int i = 1; i <= rowSize; ++i) {
+        for (int j = 1; j <= b.columnSize; ++j) {
+            C.m[i][j] = 0;
+            for (int k = 1; k <= columnSize; ++k) {
+                C.m[i][j] = (C.m[i][j] + 1LL * m[i][k] * b.m[k][j] % MOD) % MOD;
+            }
+        }
+    }
+    C.rowSize = rowSize;
+    C.columnSize = b.columnSize;
+    return C;
+}
+
+};
+ 
+Matrix Pow(Matrix a, ll n) {
+    if (n == 1) return a;
+    if (n & 1) return Pow(a,n - 1) * a;
+    Matrix tmp = Pow(a, n / 2);
+    return tmp * tmp;
+}
+
+void print(Matrix X){
+    cout << "+-";
+    FOR(i,1,X.columnSize) cout << " - ";
+    cout << "-+\n";
+
+    for (int i = 1; i <= X.rowSize; ++i) {
+        cout << "|  ";
+        for (int j = 1; j <= X.columnSize; ++j) {
+         cout << X.m[i][j] << "  ";
+        }
+        cout << "|\n";
+    }
+    
+    cout << "+-";
+    FOR(i,1,X.columnSize) cout << " - ";
+    cout << "-+\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    //freopen(file".INP","r",stdin);
+    //freopen(file".OUT","w",stdout);
+    
+    Matrix base, D;
+
+    D.rowSize = ;
+    D.columnSize = ;
+
+    // print(D);
+
+    base.rowSize = ;
+    base.columnSize = ;
+
+    // print(base);
+    
+    return 0;
+}
+
