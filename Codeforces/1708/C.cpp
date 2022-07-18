701deb90183c000277c265102d84880100f81cd1
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-07-18 09:52:34 +0700

    Create C.cpp

---
+++ Codeforces/1708/C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string str;
+typedef pair <int,int> ii;
+#define file "TEST"
+#define st first
+#define nd second
+#define pb push_back
+#define pf push_front
+#define lb lower_bound
+#define ub upper_bound
+#define er equal_range
+#define bin binary_search
+#define vll vector<ll>
+#define vi vector<int>
+#define all(v) (v).begin(), (v).end()
+#define FOR(i,x,y) for(ll i = x; i <= y; ++i)
+#define FOS(i,x,y) for(ll i = x; i >= y; --i)
+#define EACH(i,x) for (auto &(i) : (x))
+#define el cout << '\n';
+const ll MOD = 1e9 + 7;
+#define dbg(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]  "
+#define dbge(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl;
+mt19937_64 rd(chrono::steady_clock::now().time_since_epoch().count());
+ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rd); }
+
+const ll N = 1e5 + 5;
+ll a[N];
+
+void solve(){
+	ll n, q, temp_q = 0;
+	cin >> n >> q;
+	FOR(i,1,n) cin >> a[i];
+	vll res;
+	FOS(i,n,1){
+		if (temp_q < a[i]){
+			if (temp_q >= q){
+				res.pb(0);
+			}
+			else {
+				temp_q++;
+				res.pb(1);
+			}
+		}
+		else {
+			res.pb(1);
+		}
+	}
+	reverse(all(res));
+	for (auto x : res) cout << x;
+	el;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    //freopen(file".INP","r",stdin);
+    //freopen(file".OUT","w",stdout);
+
+    ll t = 1;
+    if (true) cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
